31179cd75a395cf7d0d4c248a0afbe00a004888a
Pradeep <krnprdp@gmail.com>
2017-08-06 18:53:31 -0700

    initial commit - app template

---
+++ app/src/main/java/com/pradeep/justdo_todolist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pradeep.justdo_todolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
